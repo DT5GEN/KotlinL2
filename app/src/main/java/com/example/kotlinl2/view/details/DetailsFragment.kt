87de4805279dcac01377a6afe0ea666acc59b3e4
DT5Gen <marinovod@gmail.com>
2022-03-01 05:10:53 +0300

    prototype L4

--- app/src/main/java/com/example/kotlinl2/view/details/DetailsFragment.kt
+++ app/src/main/java/com/example/kotlinl2/view/details/DetailsFragment.kt
@@ -8,11 +8,26 @@ import androidx.fragment.app.Fragment
 import com.example.kotlinl2.R
 import com.example.kotlinl2.databinding.FragmentDetailsBinding
 import com.example.kotlinl2.model.Weather
+import com.example.kotlinl2.model.WeatherDTO
 
 
 class DetailsFragment : Fragment() {
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var weatherBundle: Weather
+
+    private val onLoadListener: WeatherLoader.WeatherLoaderListener =
+        object : WeatherLoader.WeatherLoaderListener {
+
+            override fun onLoaded(weatherDTO: WeatherDTO) {
+                displayWeather(weatherDTO)
+            }
+
+            override fun onFailed(throwable: Throwable) {
+                //Обработка ошибки
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,19 +41,37 @@ class DetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        arguments?.getParcelable<Weather>(BUNDLE_EXTRA)?.let {
-            it.city.also {
-                binding.cityName.text = it.city
-                binding.cityCoordinates.text = String.format(
-                    getString(R.string.city_coordinates),
-                    it.lat.toString(),
-                    it.lon.toString()
-                )
 
-            }
-            binding.temperatureValue.text = it.temperature.toString()
-            binding.feelsLikeValue.text = it.feelsLike.toString()
+        weatherBundle = arguments?.getParcelable<Weather>(BUNDLE_EXTRA) ?: Weather()
+
+        binding.mainView.visibility = View.GONE
+        binding.loadingLayout.visibility = View.VISIBLE
+
+        val loader = WeatherLoader(onLoadListener, weatherBundle.city.lat, weatherBundle.city.lon)
+        loader.loadWeather()
+
+    }
+
+    private fun displayWeather(weatherDTO: WeatherDTO) {
+        with(binding){
+            mainView.visibility = View.VISIBLE
+            loadingLayout.visibility = View.GONE
+
+            val city = weatherBundle.city
+
+           cityName.text = city.city
+            cityCoordinates.text = String.format(
+                getString(R.string.city_coordinates),
+                city.lat.toString(),
+                city.lon.toString()
+            )
+
+            weatherCondition.text = weatherDTO.fact?.condition
+            temperatureValue.text = weatherDTO.fact?.temp.toString()
+            feelsLikeValue.text = weatherDTO.fact?.feels_like.toString()
+
         }
+
 
 
     }
